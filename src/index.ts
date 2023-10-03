9b77e8706a55f1d8890bc1106f992e20eaa73e4d
Jerell James <jerell1998@gmail.com>
2023-10-03 15:33:20 +0100

    select calendar and log upcoming events

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { authorize } from "./authorize";
-import { google } from "googleapis";
+import { calendar_v3, google } from "googleapis";
 import pathTag from "./pathTag";
+import { ArrayElement } from "./lib/arrayElement";
+import { createSelection } from "bun-promptx";
 
 // If modifying these scopes, delete token.json.
 export const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
@@ -10,7 +12,38 @@ export const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 export const TOKEN_PATH = pathTag`token.json`;
 export const CREDENTIALS_PATH = pathTag`credentials.json`;
 
-async function listCalendars() {}
+async function listCalendars(calendar: calendar_v3.Calendar) {
+  const list = await calendar.calendarList.list();
+  const items = list.data.items;
+
+  if (!items) throw new Error("no calendar items");
+
+  return items.map((c) => {
+    const { id, summary: name, description, ...details } = c;
+    return {
+      id: id as string,
+      name,
+      description,
+      details,
+    };
+  });
+}
+
+const cutoffText = (t: string, limit = 25) =>
+  t.length <= limit ? t : t.slice(0, limit - 1) + "...";
+
+async function selectCalendar(calendar: calendar_v3.Calendar) {
+  const calendars = await listCalendars(calendar);
+  const { selectedIndex, error } = createSelection(
+    calendars.map((c, i) => ({
+      text: c.name || `calendar ${i}`,
+      description: cutoffText(c.description || ""),
+    }))
+  );
+  if (error) throw new Error(error);
+  if (selectedIndex === null) throw new Error("calendar index is null");
+  return calendars[selectedIndex];
+}
 
 /**
  * Lists the next 10 events on the user's primary calendar.
@@ -18,11 +51,11 @@ async function listCalendars() {}
  */
 async function listEvents(auth: any) {
   const calendar = google.calendar({ version: "v3", auth });
-  const r = await calendar.calendarList.list();
-  console.log(r);
+  const cal = await selectCalendar(calendar);
+  console.log(cal);
 
   const res = await calendar.events.list({
-    calendarId: "primary",
+    calendarId: cal.id,
     timeMin: new Date().toISOString(),
     maxResults: 10,
     singleEvents: true,
